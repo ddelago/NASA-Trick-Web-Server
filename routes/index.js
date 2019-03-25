a0d937375f2b152b140bd0f7e82ee148a5d23706
Daniel Delago <ddelago0596@gmail.com>
2019-03-25 10:26:32 -0500

    ...

--- routes/index.js
+++ routes/index.js
@@ -3,44 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('form', {title: 'Registration form'});
+    res.send('You made a get request!');
 });
 
-router.get('/registrations', auth.connect(basic), (req, res) => {
-    Registration.find()
-        .then((registrations) => {
-            res.render('index', { title: 'Listing registrations', registrations });
-        })
-        .catch(() => {res.send('Sorry! Something went wrong.'); 
-    });
+router.post('/', (req, res) => {
+    res.send('You made a post request!');
 });
-
-router.post('/', 
-    [
-        body('name')
-            .isLength({min: 1})
-            .withMessage('Please enter a name'),
-        body('email')
-            .isLength({min: 1})
-            .withMessage('Please enter an email'),
-    ],
-
-    (req, res) => {
-        const errors = validationResult(req);
-
-        if(errors.isEmpty()) {
-            const registration = new Registration(req.body);
-            registration.save()
-                .then(() => { res.send('Thank you for your registration!'); })
-                .catch(() => { res.send('Sorry! Something went wrong.'); });
-        } else {
-            res.render('form', {
-                title: 'Registration form',
-                erros: errors.array(),
-                data: req.body,
-            });
-        }
-    }
-);
 
 module.exports = router;
