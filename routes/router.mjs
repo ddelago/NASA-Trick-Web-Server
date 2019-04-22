87013d864aa7b0ff9e5566b11a5bac636774b2cf
Daniel Delago <daniel.b.delago@nasa.gov>
2019-04-22 08:12:58 -0700

    Fixed trick var value update

--- routes/router.mjs
+++ routes/router.mjs
@@ -1,22 +1,36 @@
 import express from 'express';
 const router = express.Router();
 import { trickData } from '../common/variables';
-export { router, getVariable, getBatchVariables, addVariable };
+export { router, getVariable, getBatchVariables };
 
 // Get a variable's current value from Trick
 function getVariable(trickClient) {
-    router.get('/cmd/getVariable', (req, res) => {
+    router.get('/cmd/getVariable/*', (req, res) => {
 
-        console.log(req.body);
+        var oldValue = trickData[0];
+
+        // Extract trick variable from url
+        var trickVariable = req.url.split("/");
+        trickVariable = trickVariable[trickVariable.length - 1];
 
         // Send command to Trick
-        trickClient.write(`trick.var_add(\"${req.body.variable}\")\n`);
+        trickClient.write(`trick.var_add(\"${trickVariable}\")\n`);
 
-        res.send({"value": trickData[0]});
-    });
+        function wait() {
+            if(trickData[0] === oldValue) {
+                setTimeout(wait, 25);
+                return;
+            }
+            res.send({"value": trickData[0]});
+        }
+
+        // Wait for new value to be updated
+        wait();
+   });
 }
 
 // Create a batch request??
+// Figure out to get trick var heiarchy
 function getBatchVariables(trickClient) {
     router.get('/cmd/getVariable', (req, res) => {
         // Send command to Trick
@@ -29,13 +43,13 @@ function getBatchVariables(trickClient) {
 
 // PUT request will store value to a json object.
 // Server will then CONSTANTLY update those values from Trick.
-function addVariable(trickClient) {
-    // Add a variable to track
-    router.put('/cmd/addVariable', (req, res) => {
-        // Send command to Trick
-        trickClient.write(`trick.var_add(\"${req.body.variable}\")\n`);
+// function addVariable(trickClient) {
+//     // Add a variable to track
+//     router.put('/cmd/addVariable', (req, res) => {
+//         // Send command to Trick
+//         trickClient.write(`trick.var_add(\"${req.body.variable}\")\n`);
 
-        // Reply to trickClient
-        res.send(`Now tracking ${req.body.variable}`);
-    });
-}
+//         // Reply to trickClient
+//         res.send(`Now tracking ${req.body.variable}`);
+//     });
+// }
